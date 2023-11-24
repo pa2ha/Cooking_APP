595de9352b9b43a0d055bdd23ec5fa6e19c128ea
Даниил Дроздов <1337ddv@gmail.com>
2023-11-24 18:15:01 +0300

    test 3

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -64,9 +64,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
