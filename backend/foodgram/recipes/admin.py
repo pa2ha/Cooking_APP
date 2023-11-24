75bae810782f65edafdf8a3561c550b70d59acf9
Даниил Дроздов <1337ddv@gmail.com>
2023-11-24 18:13:50 +0300

    test 3

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -3,14 +3,12 @@ from django.contrib import admin
 from .models import Ingredient, Recipe, Tag
 
 
-# Регистрируем модель Ingredients
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'measurement_unit')
     search_fields = ('name',)
 
 
-# Регистрируем модель Tag
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'color', 'slug')
@@ -18,19 +16,16 @@ class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
-# Создаем Inline модель для RecipeIngredient
 class RecipeIngredientInline(admin.TabularInline):
     model = Recipe.ingredients.through
     extra = 1
 
 
-# Создаем Inline модель для RecipeTags
 class RecipeTagsInLine(admin.TabularInline):
     model = Recipe.tags.through
     extra = 1
 
 
-# Регистрируем модель Recipes и добавляем встроенное поле для ингредиентов
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'text', 'pub_date', 'author')
