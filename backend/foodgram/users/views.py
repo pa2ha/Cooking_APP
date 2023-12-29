ffe9f6df179f42442de9fe57ab65b7a90ea28f30
Даниил Дроздов <1337ddv@gmail.com>
2023-12-29 13:47:47 +0300

    2

--- backend/foodgram/users/views.py
+++ backend/foodgram/users/views.py
@@ -4,9 +4,9 @@ from rest_framework import status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import (SAFE_METHODS, AllowAny, BasePermission,
+                                        IsAuthenticated)
 from rest_framework.response import Response
-from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 from users.models import Subscription
 from users.pagination import CustomPageNumberPagination
